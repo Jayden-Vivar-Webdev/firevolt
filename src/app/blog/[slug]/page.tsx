427aad9ea34ed69ef98683a6f428623fbf042d4b
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2025-06-10 12:02:37 +1000

    New elements

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -95,21 +95,16 @@ const blogPosts: BlogPost[] = [
 ];
 
 interface PageProps {
-  params: {
-    slug: string;
-  };
+  params: Promise<{ slug: string }>;
 }
 
-export async function generateStaticParams() {
-  return blogPosts.map(post => ({
-    slug: post.slug,
-  }));
-}
+
 
 
 export default async function BlogPostPage({ params }: PageProps) {
+  const { slug } = await params;
 
-  const post = blogPosts.find(p => p.slug === params.slug);
+  const post = blogPosts.find(p => p.slug === slug);
 
   if (!post) {
     notFound();
